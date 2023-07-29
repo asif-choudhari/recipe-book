afed664431137cd7eb5d09b71fdea40251622485
asif-choudhari <asifahmed5@outlook.com>
2023-07-29 12:06:51 +0530

    api config change

--- src/app/shared/configs/api.config.ts
+++ src/app/shared/configs/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: 'https://recipe-book-api-delta-ten.azurewebsites.net',
+  //baseUrl: 'https://recipe-book-api-delta-ten.azurewebsites.net',
+  baseUrl: 'https://recipe-book-api-htcx.onrender.com',
   devBaseUrl: 'https://localhost:7131'
 };
